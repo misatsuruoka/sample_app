a704e6b568bce7d650b9122b0fed8ac13ae5bde8
misatsuruoka <misa.0429@docomo.ne.jp>
2022-05-25 23:45:12 +0900

    ログインユーザーの認証システムを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "the truth" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
@@ -14,9 +15,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', "The form contains 4 errors"
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
@@ -25,5 +27,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end 
+
+  test "forwarding_url must be nil after friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+
+    delete logout_path
+    get login_path
+    assert_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to @user
   end
 end
+
